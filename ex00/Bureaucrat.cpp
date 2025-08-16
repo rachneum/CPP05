4a1071bd295798ccf8742a226d319c9f31b33bf8
Klaenex <klaenex.29@gmail.com>
2025-08-16 15:32:31 +0200

    test

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -1,1 +1,22 @@
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+    std::cout << "Default constructor called." << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+{
+    std::cout << "Other bureaucrat constructor called." << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+{
+    
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Default destructor called." << std::endl;
+}
+
